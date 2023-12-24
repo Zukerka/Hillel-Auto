e6e5bd9c2689da877ad5f94b0bd2838e13331c46
Natalia Kolesnychenko <n.kolesnychenko@gmail.com>
2023-12-24 21:01:15 +0200

    eslint added

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("file:preprocessor", cypressEslint())
     },
+    baseUrl: 'https://qauto.forstudy.space'
   },
 });
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,8 @@
+/// <reference types = "cypress"/>
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.wait(1000) 
+
   })
 })
